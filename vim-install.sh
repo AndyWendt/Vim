b68fb2281ec55009ea8b16e7a7ed7ed960d8c505
Andy Wendt <andy@awendt.com>
2017-03-04 23:22:15 -0700

    Add comments to vim install command

--- vim-install.sh
+++ vim-install.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+echo "Installation Starting"
+
 cp -R ./config/.vim/* ~/.vim/
+
+echo "Copied files to ~/.vim"
+
 cp ./config/.vimrc ~/.vimrc
+
+echo "Copied ~/.vimrc"
 
 vim +PluginInstall +qall
 
