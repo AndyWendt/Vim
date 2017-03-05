d6024e6e2af8b75f652a1cd2fb6b7b0b3118abeb
Andy Wendt <andy@awendt.com>
2017-03-04 22:06:06 -0700

    Fix vim-install.sh command

--- vim-install.sh
+++ vim-install.sh
@@ -2,4 +2,8 @@
 
 cp ./config/plugins.vim ~/.vim/plugins.vim
 cp -R ./snippets ~/.vim/snippets
-cp ./config/vimrc ~/.vimrc
+cp ./config/.vimrc ~/.vimrc
+
+vim +PluginInstall +qall
+
+echo "Installation Complete"
