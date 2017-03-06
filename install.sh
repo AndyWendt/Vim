45e33a0514dc65237d7c21c3cf3b5e93d7b86be6
Andy Wendt <andy@awendt.com>
2017-03-05 23:09:27 -0700

    Fix install order

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
-
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    echo "Installed Vundle"
-fi
+echo "Installation Starting"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     cp ./config/.gtkrc-2.0 ~/.gtkrc-2.0
     echo "Copied .gtkrc-2.0"
 fi
 
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    echo "Installed Vundle"
+fi
+
+cp -R ./config/.vim/* ~/.vim/
+echo "Copied files to ~/.vim"
+
+
 cp ./config/install-vimrc ~/.vimrc
 echo "Copied Temp Install vimrc to ~/.vimrc"
 
 # install plugins
 vim +PluginInstall +qall
-
-echo "Installation Starting"
-
-cp -R ./config/.vim/* ~/.vim/
-echo "Copied files to ~/.vim"
 
 cp ./config/vimrc ~/.vimrc
 echo "Copied ~/.vimrc"
